b12323baee8b965620245d7be5444be04f893456
msraza821-crypto <shabbirraza30@gmail.com>
2020-09-18 12:20:06 +0530

    worke don mongo

--- routes/data.routes.js
+++ routes/data.routes.js
@@ -2,7 +2,7 @@ const express=require("express")
 const router=express.Router()
 const controller=require("../controllers/data.controller")
 
-router.get("/",controller.findAll)
+router.get("/api/v1/All",controller.findAll)
 
 
 module.exports=router
